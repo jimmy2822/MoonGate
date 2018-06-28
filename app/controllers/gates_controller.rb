7424490935d192e51ff7baac621b91525f6efe9d
jimmy2822 <jimmy2822@gmail.com>
2018-06-28 14:38:10 +0800

    fix n+1 query in gates page fix gates controller create failure

--- app/controllers/gates_controller.rb
+++ app/controllers/gates_controller.rb
@@ -1,10 +1,10 @@
 class GatesController < ApplicationController
-    before_action :find_gate_id, except: [:index, :show, :new, :create, :dislike, :like]
+    before_action :find_gate_id, except: [:index, :show, :new, :create]
     before_action :authenticate_user! ,only: [:edit, :destory , :new, :create]
 
     # 將 Gate Model 資料傳進實體變數 @gates
     def index
-        @gates = Gate.all
+        @gates = Gate.includes(:user, :like_logs, :taggings, :tags)
     end
 
     def show  
@@ -13,22 +13,35 @@ class GatesController < ApplicationController
 
     # 在 Controller 設定實體變數 @gate 給 view 的 form_for 使用
     def new
-        @gate = scoped.new
+        @gate = Gate.new
     end
     
     # 將已過濾的資料存進實體變數 @gate 當中
     def create
-        return redirect_to gates_path, notice: "新增門成功" if @gate.create(gate_params)
-        # 儲存失敗時借用 new 方法的頁面，不使用 redirect_to 方法使用者在輸入時的資料不會消失
-        render :new
+		@gate = Gate.new(gate_params)
+        @gate.user_id = current_user[:id]
+
+        if @gate.save
+            redirect_to gates_path, notice: "新增門成功"
+        else
+            # 儲存失敗時借用 new 方法的頁面，不使用 redirect_to 方法使用者在輸入時的資料不會消失
+			render :new, notice: "新增門失敗"
+		end
     end
 
     # 取出 Gate 資料中 id 是 params[:id] 的資料
-    def edit; end
-
-    def update
-        return redirect_to gate_path , notice: "更新成功" if @gate.update(gate_params) 
-        render :edit, notice: "編輯失敗"
+    def edit
+			
+    end
+	
+	def update
+		if @gate.update(gate_params)
+			@gate.tag_list.add(params[:tag])
+			@gate.save
+			redirect_to gates_path, notice: "編輯 SUCCESS!"
+		else	
+			render :edit, notice: "編輯失敗"
+		end
     end
 
     def destroy
@@ -37,19 +50,10 @@ class GatesController < ApplicationController
         # redirect_to gates_path, notice: "刪除成功"
     end
 
-    def like
-        Gate.find(params[:id]).like_logs.create(ip_address: request.remote_ip,who_liked: current_user[:id]) if @gate
-        redirect_to gates_path, notice: "喜歡成功!"
-    end
-
-    def dislike
-        @gate.destroy if @gate.like_logs.where(who_liked:current_user[:id])
-    end
-
     private
     # 利用 Strong Parameters 設定過濾參數
     def gate_params
-        params.require(:gate).permit(:name, :icon, :tag, :intro, :intro_detail, :is_public, :like, :server)
+        params.require(:gate).permit(:name, :icon, :intro, :intro_detail, :is_public, :like, :server, :tag_list, :user_id)
     end
     # 每個 Action 撈出 route 對應資料庫 id 的參數
     def find_gate_id
@@ -57,7 +61,7 @@ class GatesController < ApplicationController
     end
 
     def scoped
-        return Gate if current_user.admin? && ['update', 'edit'].include?(params[:action])
+        return Gate if current_user.admin? && ['update', 'edit', 'destroy'].include?(params[:action])
         current_user.gates
     end
 end
--- app/models/gate.rb
+++ app/models/gate.rb
@@ -1,4 +1,7 @@
 class Gate < ApplicationRecord
     has_many :like_logs, dependent: :destroy
     belongs_to :user, foreign_key:"user_id"
+    
+    # Gate as taggable
+    acts_as_taggable
 end
