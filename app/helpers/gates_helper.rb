e3795ab0993341df027144a107217a04d978bd14
jimmy2822 <jimmy2822@gmail.com>
2018-06-29 14:19:25 +0800

    add search bar for searching taged gates

--- app/helpers/gates_helper.rb
+++ app/helpers/gates_helper.rb
@@ -2,9 +2,9 @@ module GatesHelper
     # 如果使用者為登入狀態，就顯示登出按鈕，否則顯示登入按鈕
     def sign_out
         if user_signed_in?
-            link_to "Sign out", destroy_user_session_path, method: "delete"
+            link_to "會員登出", destroy_user_session_path, method: "delete"
         else
-            link_to "Sign in", user_session_path
+            link_to "會員登入", user_session_path
         end
     end
 
