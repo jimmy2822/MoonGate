c2119caf8a42690b7b7593c6b83c541d1d4b9e20
jimmy2822 <jimmy2822@gmail.com>
2018-07-18 14:10:40 +0800

    add gem faker for seeding testing datas

--- Gemfile
+++ Gemfile
@@ -71,6 +71,10 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'guard-livereload', '~> 2.5', require: false
+
+  # Generate testing data
+  gem 'faker'
+
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10000.times do |fake|
+    Gate.create!(:name => Faker::Lorem.words(3) , :intro => Faker::Lorem.sentence(20, true),user_id:1 ,server:"666",is_public:"1" )
+end
+
